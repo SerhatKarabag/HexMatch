8cbebc8f9687830a65d83729440066aa479c3008
Serhat Karabag <skarabag4@gmail.com>
2020-02-28 10:48:42 +0300

    Upload

---
+++ Assets/Scripts/Hexagon.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hexagon : SuperClass {
+	GridManager GridManagerObject;
+	public int x;
+	public int y;
+	public Color color;
+	public Vector2 lerpPosition;
+	public bool lerp;
+	public Vector2 speed;
+	private bool bomb;
+	private int bombTimer;
+	private TextMesh text;
+	public bool isLocated;
+
+
+	/* Struct to hold neighboor grid coordinates */
+	public struct NeighbourHexes {
+		public Vector2 up;
+		public Vector2 upLeft;
+		public Vector2 upRight;
+		public Vector2 down;
+		public Vector2 downLeft;
+		public Vector2 downRight;
+	}
+
+	void Start() {
+		GridManagerObject = GridManager.instance;
+		lerp = false;
+		isLocated = false;
+	}
+
+	void Update() {
+		if (lerp) {
+			float newX = Mathf.Lerp(transform.position.x, lerpPosition.x, Time.deltaTime*HEXAGON_ROTATE_CONSTANT);
+			float newY = Mathf.Lerp(transform.position.y, lerpPosition.y, Time.deltaTime*HEXAGON_ROTATE_CONSTANT);
+			transform.position = new Vector2(newX, newY);
+
+			
+			if (Vector3.Distance(transform.position, lerpPosition) < HEXAGON_ROTATE_THRESHOLD) {
+				transform.position = lerpPosition;
+				lerp = false;
+				isLocated = true;
+			}
+		}
+	}
+
+
+	/* Function to save rotate changes */
+	public void Rotate(int newX, int newY, Vector2 newPos) {
+		lerpPosition = newPos;
+		SetX(newX);
+		SetY(newY);
+		lerp = true;
+		isLocated = false;
+	}
+
+	/* Builds a struct from grid position of neighbour hexagons and returns it */
+	public NeighbourHexes GetNeighbours() {
+		NeighbourHexes neighbours;
+		bool onStepper = GridManagerObject.OnStepper(GetX());
+
+
+		neighbours.down = new Vector2(x, y-1);
+		neighbours.up = new Vector2(x, y+1);
+		neighbours.upLeft = new Vector2(x-1, onStepper ? y+1 : y);
+		neighbours.upRight = new Vector2(x+1, onStepper ? y+1 : y);
+		neighbours.downLeft = new Vector2(x-1, onStepper ? y : y-1);
+		neighbours.downRight = new Vector2(x+1, onStepper ? y : y-1);
+
+
+		return neighbours;
+	}
+
+
+	/* Set new world position for hexagon */
+	public void ChangeWorldPosition(Vector2 newPosition) {
+		lerpPosition = newPosition;
+		lerp = true;
+		isLocated = false;
+	}
+
+
+	/* Set new grid position for hexagon */
+	public void ChangeGridPosition(Vector2 newPosition) {
+		x = (int)newPosition.x;
+		y = (int)newPosition.y;
+	}
+
+	public void SetBomb() {
+		text = new GameObject().AddComponent<TextMesh>();
+		text.alignment = TextAlignment.Center;
+		text.anchor = TextAnchor.MiddleCenter;
+		text.transform.localScale = transform.localScale - (transform.localScale/3);
+		text.transform.position = new Vector3(transform.position.x, transform.position.y, -4);
+		text.color = Color.black;
+		text.transform.parent = transform;
+		bombTimer = BOMB_TIMER_START;
+		text.text = bombTimer.ToString();
+	}
+
+
+	/* Setters & Getters */
+	public void SetX(int value) { x = value; }
+	public void SetY(int value) { y = value; }
+	public void SetColor(Color newColor) { GetComponent<SpriteRenderer>().color = newColor; color=newColor; }
+	public void Tick() { --bombTimer; text.text = bombTimer.ToString(); }
+	public int GetX() { return x; }
+	public int GetY() { return y; }
+	public Color GetColor() { return GetComponent<SpriteRenderer>().color; }
+	public int GetTimer () { return bombTimer; }
+}
---
+++ Assets/Scripts/UserInterfaceManager.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Linq;
+
+public class UserInterfaceManager : SuperClass
+{
+    public Text score;
+    public Text widthValueText;
+    public Text heightValueText;
+    public Text colorCountText;
+    public Slider widthSlider;
+    public Slider heightSlider;
+    public Slider colorCountSlider;
+    public GameObject preparationScreen;
+    public GameObject colorSelectionParent;
+    public GameObject gameOverScreen;
+    public List<Color> availableColors;
+   
+
+    private GridManager GridManagerObject;
+    private int colorCount;
+    private int blownHexagons;
+    private int bombCount;
+
+    public static UserInterfaceManager instance;
+
+
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+    }
+
+    void Start()
+    {
+        bombCount = ZERO;
+        GridManagerObject = GridManager.instance;
+        blownHexagons = ZERO;
+        colorCount = 7;
+        InitializeUI();
+    }
+
+    public void Score(int x)
+    {
+        blownHexagons += x;
+        score.text = (SCORE_CONSTANT * blownHexagons).ToString(); // Scoring is 5 times of blown hexagons
+        if (Int32.Parse(score.text) > BOMB_SCORE_THRESHOLD * bombCount + BOMB_SCORE_THRESHOLD) // Need 1000 point for every bomb
+        {
+            ++bombCount;
+            GridManagerObject.SetBombProduction();
+        }
+    }
+
+    public void GameEnd()
+    {
+        gameOverScreen.SetActive(true);
+    }
+
+    public void BackButton(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void WidthSliderChange()
+    {
+        /* This will allow only odd numbers to protect symmetrical visual */
+        widthValueText.text = ((widthSlider.value - MINIMUM_GRID_WIDTH) * DOUBLE + MINIMUM_GRID_WIDTH).ToString();
+    }
+
+    public void HeightSliderChange()
+    {
+        heightValueText.text = heightSlider.value.ToString();
+    }
+
+    public void ColorCountSliderChange()
+    {
+        int childCount = colorSelectionParent.transform.childCount;
+        int newCount = (int)colorCountSlider.value;
+        colorCountText.text = newCount.ToString();
+
+
+        if (newCount > colorCount)
+        {
+            for (int i = 0; i < newCount; ++i)
+            {
+                if (!colorSelectionParent.transform.GetChild(i).gameObject.activeSelf)
+                {
+                    colorSelectionParent.transform.GetChild(i).gameObject.SetActive(true);                 
+                }
+            }
+        }
+
+        else if (newCount < colorCount)
+        {
+            for (int i = colorCount-1; i >= newCount; --i)
+            {
+                colorSelectionParent.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+
+        colorCount = newCount;
+    }
+
+
+
+    /* Sends options to required objects and starts game */
+    public void StartGameButton()
+    {
+        preparationScreen.SetActive(false);
+        GridManagerObject.SetGridHeight((int)heightSlider.value);
+        GridManagerObject.SetGridWidth((int)(widthSlider.value - MINIMUM_GRID_WIDTH) * DOUBLE + MINIMUM_GRID_WIDTH);
+        GridManagerObject.Colors = GetColorList();
+        GridManagerObject.SetColorList(GridManagerObject.Colors.Take((int)colorCountSlider.value).ToList());
+        GridManagerObject.InitializeGrid();
+    }
+
+   
+    public List<Color> GetColorList()
+    {
+
+        List<Color> colors = new List<Color>();
+
+        colors.Add(Color.blue);
+        colors.Add(Color.red);
+        colors.Add(Color.yellow);
+        colors.Add(Color.green);
+        colors.Add(Color.cyan);
+        colors.Add(Color.magenta);
+        colors.Add(Color.white);
+
+        return colors;
+    }
+
+    /* Sets default values to UI elements */
+    private void InitializeUI()
+    {
+        Default();
+        for (int i = 0; i < colorSelectionParent.transform.childCount - colorCount; ++i)
+        {
+            colorSelectionParent.transform.GetChild(colorSelectionParent.transform.childCount - i - 1).gameObject.SetActive(false);
+        }
+    }
+    /* Assigns all options to default values */
+    public void Default()
+    {
+        heightSlider.value = DEFAULT_GRID_HEIGHT;
+        widthSlider.value = DEFAULT_GRID_WIDTH;
+        colorCountSlider.value = DEFAULT_COLOR_COUNT;
+        colorCount = DEFAULT_COLOR_COUNT;
+        widthValueText.text = ((widthSlider.value - MINIMUM_GRID_WIDTH) * DOUBLE + MINIMUM_GRID_WIDTH).ToString();
+        heightValueText.text = heightSlider.value.ToString();
+        score.text = blownHexagons.ToString();
+    }
+}
